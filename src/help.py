d3daacef0fe43333b478262c0c6d8a803384e38c
UserStalker <98112013+UserZtalker@users.noreply.github.com>
2022-01-27 22:58:23 +0100

    Update help.py

--- src/help.py
+++ src/help.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from config import PREFIX
 
 class cmd(commands.Cog):
     def __init__(self, bot):
@@ -10,15 +9,11 @@ class cmd(commands.Cog):
     async def help(self, ctx):
         author = ctx.message.author
         pfp = author.avatar_url
-
-        await ctx.message.delete()
-        embed=discord.Embed(title="List", description=f"Don't forget to type `{PREFIX}`", color=0xD22024)
+        embed = discord.Embed(title='Help', color=0xcc3030)
         embed.set_author(name=author, icon_url=pfp)
-        embed.add_field(name="`[>] fun`:", value='**"FUN"** commands', inline=True)
-        embed.add_field(name="`[>] nsfw`:", value='**NSFW** commands', inline=True)
-        embed.add_field(name="`[>] haxor`:", value='**HAXOR** commands', inline=True)
-        embed.set_image(url="https://media2.giphy.com/media/MM0Jrc8BHKx3y/giphy.gif?cid=ecf05e47yw3wa4kn3eoi7aknziii46yc7rukw80geqf5hbek&rid=giphy.gif&ct=g")
-        embed.set_footer(text="This message will be deleted in 15 seconds")
+        embed.add_field(name="- fun", value='Fun commands', inline=True)
+        embed.add_field(name="- nsfw", value='nsfw commands', inline=True)
+        embed.add_field(name="- hacker", value='"Hacker" commands', inline=True)
         await ctx.send(embed=embed, delete_after=15.0)
 
         print(f"[>]HELP was executed")
@@ -28,16 +23,12 @@ class cmd(commands.Cog):
     async def fun(self, ctx):
         author = ctx.message.author
         pfp = author.avatar_url
-        
-        await ctx.message.delete()
-        embed=discord.Embed(title="Fun", description=f"Don't forget to use {PREFIX}", color=0xe55757)
+        embed = discord.Embed(title='Fun', color=0x11d84d)
         embed.set_author(name=author, icon_url=pfp)
-        embed.add_field(name="`[>] gp`", value="Ghostping/delete msg after sending it", inline=True)
-        embed.add_field(name="`[>] spam <amount>`", value="Spam any message you like 100 times", inline=True)
-        embed.add_field(name="`[>] ascii <input>`", value="Send any word in ascii", inline=True)
-        embed.add_field(name="`[>] server`", value="Server Information", inline=True)
-        embed.add_field(name="`[>] iplookup <input>`", value="Checks data of choosen ip", inline=True)
-        embed.set_footer(text="This message will be deleted in 15 seconds")
+        embed.add_field(name="- ip <input>", value='Checks information of given IP', inline=True)
+        embed.add_field(name="- embed <text>", value='Send\'s given string(text) in embed', inline=True)
+        embed.add_field(name="- cd <text>", value='Send\'s given string(text) in codeblock', inline=True)
+        embed.add_field(name="- spam <text>", value='spam\'s given text. If no text is set the command will be executed with "spam" set as a message', inline=True)
         await ctx.send(embed=embed, delete_after=15.0)
 
         print(f"[>]FUN was executed")
@@ -46,28 +37,21 @@ class cmd(commands.Cog):
     async def nsfw(self, ctx):
         author = ctx.message.author
         pfp = author.avatar_url
-        
-        await ctx.message.delete()
-        embed=discord.Embed(title="NSFW", description=f"Don't forget to use {PREFIX}\nrule34 isn't working in this version", color=0xa72ffc)
+        embed = discord.Embed(title='NSFW', color=0xea09c5)
         embed.set_author(name=author, icon_url=pfp)
-        embed.add_field(name="`[>] Rule34 <input> / Rule34 <input> <amount>`", value="Rule34 of your choosen \"input\"")
-        embed.add_field(name="`[>] minus8`", value="Minus8 dance", inline=True)
-        embed.set_footer(text="This message will be deleted in 15 seconds")
+        embed.add_field(name="- r34 <input>", value='Classic nsfw command', inline=True)
         await ctx.send(embed=embed, delete_after=15.0)
-
+        
         print(f"[>]NSFW was executed")
     
     @commands.command()
     async def haxor(self, ctx):
         author = ctx.message.author
         pfp = author.avatar_url
-        
-        await ctx.message.delete()
-        embed=discord.Embed(title="Haxor", description=f"Don't forget to use {PREFIX}")
+        embed = discord.Embed(title='Help', color=0x070707)
         embed.set_author(name=author, icon_url=pfp)
-        embed.add_field(name="`[>] createchannel <input>`", value="Create's channel", inline=True)
-        embed.add_field(name="`[>] nuke <input>`", value="Delete's all channels and make's new one's", inline=True)
-        embed.set_footer(text="This message will be deleted in 15 seconds")
+        embed.add_field(name="- raid <input>", value='If string(input) isn\'t given this command will be executed anyways', inline=True)
+        embed.add_field(name="- cch", value='Create\'s channel with your string(input) if input isn\'t given this command will be executed anyways', inline=True)
         await ctx.send(embed=embed, delete_after=15.0)
 
         print(f"[>]FUN was executed")
